faae6598d3670984e3fdc4623c554b28fd651084
Dannyboywoop <DanielJHolmes@hotmail.co.uk>
2018-05-19 15:51:40 +0100

    condensed into one function for speed

--- Collatz/Source.cpp
+++ Collatz/Source.cpp
@@ -2,67 +2,44 @@
 #include<chrono>
 using namespace std;
 
-int* chains;
-int goal;
+int longestStart;
+int longestLength;
 
 void applyCollatz(unsigned long int& n) {
 	n = move((n % 2 == 0) ? n / 2 : n * 3 + 1);
 }
 
-int calculateChain(unsigned long int startPos) {
-	int chainLength{ 1 };
-	unsigned long int n = startPos;
-	while (n != 1) {
-		applyCollatz(n);
-		chainLength++;
-	}
-	return chainLength;
-}
-
-void performRun() {
-	for (int i = 1; i <= goal; i++) {
-		chains[i-1]=calculateChain(i);
-	}
-}
-
-int getLongest() {
-	int longestIndex = 0;
-	int longestVal = 0;
-	for (int i = 1; i <= goal; i++) {
-		if (chains[i] > longestVal) {
-			longestIndex = i;
-			longestVal = chains[i];
+void getLongestChain(unsigned long int goal) {
+	for (unsigned int i = 1; i <= goal; i++) {
+		int chainLength{ 1 };
+		unsigned long int n = i;
+		while (n != 1) {
+			applyCollatz(n);
+			chainLength++;
+		}
+		if (chainLength > longestLength) {
+			longestLength = chainLength;
+			longestStart = i;
 		}
 	}
-	return longestIndex;
 }
 
 int main() {
 	// Record start time
 	auto start = std::chrono::high_resolution_clock::now();
-
-	goal = 10000000;
-	try {
-		chains = new int[goal]();
-	}
-	catch (bad_alloc &e) {
-		cout << e.what() << endl;
-		return 1;
-	}
-	performRun();
-	int longestChain = getLongest();
+	int goal = 1000000;
+	getLongestChain(goal);
 
 	// Record end time
 	auto finish = std::chrono::high_resolution_clock::now();
 
 	//output chain details
 	cout << "For values less than " << goal << endl;
-	cout << "Longest chain starts at: " << longestChain + 1
-		<< ", it is " << chains[longestChain] << " long!" << endl;
+	cout << "Longest chain starts at: " << longestStart
+		<< ", it is " << longestLength << " long!" << endl;
 
 	//measure time elapsed
 	std::chrono::duration<double> elapsed = finish - start;
 	cout << "Time for execution: " << elapsed.count() << "s" << endl;
 
-	delete[] chains;
 }
