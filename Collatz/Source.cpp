c923635554e25c3d90057924ac2bb0c2b6f8c8df
Dannyboywoop <DanielJHolmes@hotmail.co.uk>
2018-05-19 17:24:42 +0100

    Added more comments

--- Collatz/Source.cpp
+++ Collatz/Source.cpp
@@ -3,9 +3,12 @@
 #include<string>
 using namespace std;
 
-//VS is fickle with what theyll let me allocate, so just the biggest number that consistantly worked (bigger would improve speed)
+//VS is fickle with what theyll let me allocate,
+//so just the biggest number that consistantly worked (bigger would improve speed)
 const unsigned int arraySize{ 320000000 };
-int* chains;//global array to store chain lengths, chains[i] repesents the chainlength starting at position i+1;
+//global array to store chain lengths,
+//chains[i] repesents the chainlength starting at position i+1;
+int* chains;
 
 //simply gets next item in collatz sequence
 unsigned long int applyCollatz(unsigned long int n) {
@@ -23,13 +26,14 @@ int calculateChain(unsigned long int startPos) {
 		}
 		return chains[startPos - 1];//return chainLength
 	}
-	//if out of range of array in which chain lengths are stored, just calculate the chain length and return
+	//if out of range of array in which chain lengths are stored,
+	//just calculate the chain length based on next in sequence and return
 	return move(calculateChain(applyCollatz(startPos)) + 1);
 }
 
 //find the starting pos < goal with the longest chain
 int findLongestChain(int goal) {
-	int longestChainIndex = 0;
+	int longestChainStart = 0;
 	int longestChain = 0;
 	int bottomOfSearch = goal / 2;//longest chain will be in upper half
 	if (bottomOfSearch % 2 == 0) bottomOfSearch++;//start on an odd number
@@ -39,45 +43,45 @@ int findLongestChain(int goal) {
 		//calculate the chain length and see if its the longest
 		if (calculateChain(i) > longestChain) {
 			//if so store its details
-			longestChainIndex = i-1;
+			longestChainStart = i;
 			longestChain = chains[i-1];
 		}
 	}
 	//return the startPos of longest chain
-	return longestChainIndex;
+	return longestChainStart;
 }
 
 
 int main() {
-	// Record start time
-
 	try {
-		chains = new int[arraySize]();
+		chains = new int[arraySize]();//initialize empty array
 	}
-	catch (bad_alloc &e) {
+	catch (bad_alloc &e) {//catch possible failure to allocate memory to array
 		cout << e.what() << endl;
 		return 1;
 	}
 
 	int goal;
 	cout << "Enter number to iterate up to (dont be a cunt, this isn't parsed): ";
-	cin >> goal;
-	if (goal < 10) {
+	cin >> goal;//take user input of goal
+	if (goal < 10) {//make sure its above 10
 		cout << "Goal must be greater than 10!" << endl;
 		return 1;
 	}
 
+	// Record start time
 	auto start = std::chrono::high_resolution_clock::now();
 	
-	int longestChain = findLongestChain(goal);
+	//find longest chain
+	int longestChainStart = findLongestChain(goal);
 
 	// Record end time
 	auto finish = std::chrono::high_resolution_clock::now();
 
 	//output chain details
 	cout << "For values less than " << goal << endl;
-	cout << "Longest chain starts at: " << longestChain + 1
-		<< ", it is " << chains[longestChain] << " long!" << endl;
+	cout << "Longest chain starts at: " << longestChainStart
+		<< ", it is " << chains[longestChainStart] << " long!" << endl;
 
 	//measure time elapsed
 	std::chrono::duration<double> elapsed = finish - start;
@@ -85,6 +89,6 @@ int main() {
 	string confirm;
 	cout << endl << "Hit return to close the Program!";
 	cin.ignore();
-	getline(cin, confirm);
+	getline(cin, confirm);//pause for user input
 	delete[] chains;
 }
